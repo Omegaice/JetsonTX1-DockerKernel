b0c584bb2318531c29a89e587fdd3dbf6ccf1e45
James Sweet <james.sweet88@googlemail.com>
2016-07-17 14:17:40 -0400

    Update to 24.0.1

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 set -e
 
 ROOT=$PWD
-KERNEL_VERSION=3.10.67-g458d45c
 
 # if the user is not root, there is not point in going forward
 THISUSER=`whoami`
@@ -25,11 +24,15 @@ export ARCH=arm64
 
 # Download Tegra driver scripts and root filesystem
 if [ ! -f Scripts.tbz2 ]; then
-    curl -o Scripts.tbz2 http://developer.download.nvidia.com/embedded/L4T/r23_Release_v2.0/Tegra210_Linux_R23.2.0_armhf.tbz2
+    curl -o Scripts.tbz2 http://developer.download.nvidia.com/embedded/L4T/r24_Release_v1.0/Vulkan_Beta/Tegra210_Linux_R24.0.1_armhf.tbz2
 fi
 
 if [ ! -f Filesystem.tbz2 ]; then
-    curl -o Filesystem.tbz2 http://developer.download.nvidia.com/embedded/L4T/r23_Release_v2.0/Tegra_Linux_Sample-Root-Filesystem_R23.2.0_armhf.tbz2
+    curl -o Filesystem.tbz2 http://developer.download.nvidia.com/embedded/L4T/r24_Release_v1.0/Vulkan_Beta/Tegra_Linux_Sample-Root-Filesystem_R24.0.1_armhf.tbz2
+fi
+
+if [ ! -f Kernel.tbz2 ]; then
+    curl -o Kernel.tbz2 http://developer.download.nvidia.com/embedded/L4T/r24_Release_v1.0/Vulkan_Beta/source/kernel_src.tbz2
 fi
 
 # Destroy old versions
@@ -50,18 +53,20 @@ tar jxpf ../../Filesystem.tbz2
 mkdir -p $TEGRA_ROOT/sources
 cd $TEGRA_ROOT/sources
 git clone --depth 1 --branch tegra-l4t-r23.2 git://nv-tegra.nvidia.com/linux-3.10.git kernel
+rm -rf kernel/*
+tar xjf $ROOT/Kernel.tbz2
 cd $TEGRA_ROOT/sources/kernel
 
 # Patch Kernel
-git apply < $ROOT/vdso_arm_compile.patch
-git apply < $ROOT/remove_dirty_postfix.patch
-git apply < $ROOT/kernel_cgroup_uid.patch
-git apply < $ROOT/fs_proc_base_uid.patch
-git apply < $ROOT/drivers_misc_profiler_uid.patch
-git apply < $ROOT/include_net_route_uid.patch
-git apply < $ROOT/net_ipv4_route_uid.patch
-git apply < $ROOT/net_filter_xt_qtaguid_uid.patch
-git apply < $ROOT/net_filter_xt_quota2_uid.patch
+cat $ROOT/vdso_arm_compile.patch | patch -p1
+#cat $ROOT/remove_dirty_postfix.patch | patch -p1
+#cat $ROOT/kernel_cgroup_uid.patch | patch -p1
+#cat $ROOT/fs_proc_base_uid.patch | patch -p1
+#cat $ROOT/drivers_misc_profiler_uid.patch | patch -p1
+#cat $ROOT/include_net_route_uid.patch | patch -p1
+#cat $ROOT/net_ipv4_route_uid.patch | patch -p1
+#cat $ROOT/net_filter_xt_qtaguid_uid.patch | patch -p1
+#cat $ROOT/net_filter_xt_quota2_uid.patch | patch -p1
 
 # Setup AUFS
 git clone --depth 1 --branch aufs3.10.x git://git.code.sf.net/p/aufs/aufs3-standalone
@@ -71,12 +76,14 @@ rm include/uapi/linux/Kbuild
 cp -rp *.patch fs include Documentation ../
 
 cd $TEGRA_ROOT/sources/kernel
-git apply < aufs3-kbuild.patch 
-git apply < aufs3-base.patch 
-git apply < aufs3-mmap.patch 
-git apply < aufs3-standalone.patch
+cat aufs3-kbuild.patch | patch -p1
+cat aufs3-base.patch | patch -p1
+cat aufs3-mmap.patch | patch -p1
+cat aufs3-standalone.patch | patch -p1
 
 rm -rf aufs3-standalone *.patch
+
+exit
 
 # Build Kernel
 mkdir -p $TEGRA_KERNEL_OUT
@@ -94,12 +101,6 @@ cp $TEGRA_KERNEL_OUT/arch/arm64/boot/zImage .
 cd $TEGRA_ROOT/kernel/dtb
 cp $TEGRA_KERNEL_OUT/arch/arm64/boot/dts/*.dtb .
 
-# Copy Extra Modules
-cd $TEGRA_ROOT/kernel
-tar xvf kernel_supplements.tbz2
-cp -r $TEGRA_ROOT/kernel/lib/modules/$KERNEL_VERSION/extra $TEGRA_MODULE_OUT/lib/modules/$KERNEL_VERSION/
-rm -rf $TEGRA_ROOT/kernel/lib kernel_suppliments.tbz2
-
 # Compress Resulting Modules
 cd $TEGRA_MODULE_OUT
 tar cjf kernel_supplements.tbz2 lib
@@ -109,15 +110,6 @@ mv kernel_supplements.tbz2 $TEGRA_ROOT/kernel
 cd $TEGRA_ROOT
 ./apply_binaries.sh
 
-# Get cgroupfs-mount
-git clone --depth 1 https://github.com/tianon/cgroupfs-mount.git
-cp cgroupfs-mount/cgroupfs-mount $TEGRA_ROOT/rootfs/usr/bin
-cp cgroupfs-mount/cgroupfs-umount $TEGRA_ROOT/rootfs/usr/bin
-cp cgroupfs-mount/debian/cgroupfs-mount.upstart $TEGRA_ROOT/rootfs/etc/init/cgroupfs-mount.conf
-rm -rf cgroupfs-mount.git
-
 # Flash Kernel
 ./flash.sh jetson-tx1 mmcblk0p1
-
-
 
